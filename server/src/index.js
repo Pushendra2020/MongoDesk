ce0b7d1f8f35c015957618558b31c37f97e93b35
Pushendra2020 <pushendravedak2020@gmail.com>
2025-08-17 20:21:48 +0530

    almost

--- server/src/index.js
+++ server/src/index.js
@@ -7,7 +7,11 @@ import authRoutes from "./routes/auth.js";
 import chatRoutes from "./routes/chat.js";
 
 const app = express();
-app.use(cors({ origin: process.env.CLIENT_ORIGIN?.split(",") || "*", credentials: false }));
+app.use(cors({
+    origin:process.env.CLIENT_ORIGIN,
+    credentials:true
+}))
+//app.use(cors({ origin: process.env.CLIENT_ORIGIN?.split(",") || "*", credentials: false }));
 app.use(express.json({ limit: "4mb" }));
 app.use(express.urlencoded({ extended: true }));
 
